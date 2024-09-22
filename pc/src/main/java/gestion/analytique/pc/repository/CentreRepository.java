83cfd7cc0e06f4437d14ae1e98ccdb2b6b7e058b
akapapillio <159757534+akapapillio@users.noreply.github.com>
2024-09-22 00:29:53 +0000

    Base html

--- pc/src/main/java/gestion/analytique/pc/repository/CentreRepository.java
+++
@@ -1,7 +0,0 @@
-package gestion.analytique.pc.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import gestion.analytique.pc.model.Centre;
-
-public interface CentreRepository extends CrudRepository<Centre, Long> {
-}
