a40dfb0e808e6ed3b4e9bb9466eeb59e06ed5897
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-09-23 03:41:56 +0200

    init pc_webapp

---
+++ webapp/src/test/java/mg/jca/webapp/demo_webapp/DemoWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.jca.webapp.demo_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
