86fb7586ab2948d57130f212f8f91e51accc6b5a
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-09-26 09:46:45 +0200

    update script sql

--- base/shema.sql
+++ base/shema.sql
@@ -64,12 +64,26 @@ CREATE TABLE Centre(
    FOREIGN KEY(id_unit_oeuvre) REFERENCES UnitOeuvre(id_unit_oeuvre)
 );
 
+CREATE TABLE Charge(
+   id_charge INTEGER,
+   total_montant NUMERIC(15,2)  ,
+   date_charge DATE NOT NULL,
+   id_rubrique INTEGER NOT NULL,
+   PRIMARY KEY(id_charge),
+   FOREIGN KEY(id_rubrique) REFERENCES Rubrique(id_rubrique)
+);
+
 CREATE TABLE Detail_charge(
+   id_nature INTEGER,
+   id_centre INTEGER,
+   id_charge INTEGER,
    id_charge_detail INTEGER,
    montant NUMERIC(15,2)  ,
-   id_centre INTEGER NOT NULL,
-   PRIMARY KEY(id_charge_detail),
-   FOREIGN KEY(id_centre) REFERENCES Centre(id_centre)
+   cles_repartition VARCHAR(50) ,
+   PRIMARY KEY(id_nature, id_centre, id_charge, id_charge_detail),
+   FOREIGN KEY(id_nature) REFERENCES Nature(id_nature),
+   FOREIGN KEY(id_centre) REFERENCES Centre(id_centre),
+   FOREIGN KEY(id_charge) REFERENCES Charge(id_charge)
 );
 
 CREATE TABLE CentreProduction(
@@ -91,13 +105,4 @@ CREATE TABLE Repartition_charge_centre(
    PRIMARY KEY(id_centre, id_rubrique, id_repartition_charge_centre),
    FOREIGN KEY(id_centre) REFERENCES Centre(id_centre),
    FOREIGN KEY(id_rubrique) REFERENCES Rubrique(id_rubrique)
-);
-
-CREATE TABLE Charge(
-   id_charge INTEGER,
-   total_montant NUMERIC(15,2)  ,
-   date_charge DATE NOT NULL,
-   id_charge_detail INTEGER NOT NULL,
-   PRIMARY KEY(id_charge),
-   FOREIGN KEY(id_charge_detail) REFERENCES Detail_charge(id_charge_detail)
 );
